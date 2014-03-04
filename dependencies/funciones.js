080c48cdc02aeb8d5276eb1e2d7fc43fa09fcd0a
Oscar Bravo Sarro <osquitarbs@gmail.com>
2014-03-04 20:39:07 +0100

    creada version app

--- dependencies/funciones.js
+++ dependencies/funciones.js
@@ -1,4 +1,4 @@
-var ruta_inicial="http://bravveb.tuars.com/lista_compra/php/"; 
+var ruta_inicial="http://bravveb.hol.es/lstcp/php/"; 
 $(window).load(function(){
     setTimeout(function() {
         if (localStorage.getItem("identificador_sesion").length === 0)
--- php/conexion.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conexion = mysql_connect ("mysql.tuars.com","u948126023_lstc","bravveb0115");
-mysql_select_db("u948126023_lstc",$conexion);
